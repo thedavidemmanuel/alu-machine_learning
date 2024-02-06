e2a6fdcce18ab0bb237b7d4598f66fa844707513
thedavidemmanuel <sirdavidemmanuel@gmail.com>
2024-02-06 06:32:31 +0200

    Add task 0 Slice Me Up script

--- math/linear_algebra/0-slice_me_up.py
+++ math/linear_algebra/0-slice_me_up.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
-arr = [9, 8, 2, 3, 9, 4, 1, 0, 3]
-arr1, arr2, arr3 = arr[:2], arr[-5:], arr[1:6]
+arr = [9, 8, 2, 3, 9, 4, 1, 0, 3]  # Original array
+arr1 = arr[:2]  # The first two numbers
+arr2 = arr[-5:]  # The last five numbers
+arr3 = arr[1:6]  # The 2nd through 6th numbers
+# Print the results
 print("The first two numbers of the array are: {}".format(arr1))
 print("The last five numbers of the array are: {}".format(arr2))
 print("The 2nd through 6th numbers of the array are: {}".format(arr3))
