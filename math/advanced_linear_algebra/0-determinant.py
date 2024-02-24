ae7234583003db888aad13bfee2436bdd769a212
thedavidemmanuel <sirdavidemmanuel@gmail.com>
2024-02-25 00:15:36 +0200

    update

--- math/advanced_linear_algebra/0-determinant.py
+++ math/advanced_linear_algebra/0-determinant.py
@@ -46,5 +46,4 @@ def determinant(matrix):
         minor = [row[:col] + row[col + 1:] for row in matrix[1:]]
         cofactor = (-1) ** col * matrix[0][col] * determinant(minor)
         det += cofactor
-
     return det
