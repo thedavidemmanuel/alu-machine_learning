db359b3819b1e5dfdbbfcfeea2bc5896d696fa6e
thedavidemmanuel <sirdavidemmanuel@gmail.com>
2024-02-24 23:08:44 +0200

    update

--- math/advanced_linear_algebra/5-definiteness.py
+++ math/advanced_linear_algebra/5-definiteness.py
@@ -1,65 +1,40 @@
 #!/usr/bin/env python3
 """
-Module to calculate the definiteness of a given square matrix.
+This module defines a function that calculates the definiteness of a matrix.
 """
 import numpy as np
-
 
 def definiteness(matrix):
     """
     Calculates the definiteness of a matrix.
 
     Args:
-        matrix (numpy.ndarray): The matrix whose definiteness should be calculated.
+    matrix (numpy.ndarray): The matrix whose definiteness should be calculated.
 
     Returns:
-        str: The definiteness of the matrix, or None if the matrix does not fit
-        any known category.
+    str: Description of matrix definiteness, or None if conditions aren't met.
     """
     if not isinstance(matrix, np.ndarray):
-        raise TypeError("matrix must be a numpy.ndarray")
-    if matrix.size == 0:
-        return None
-    if matrix.shape[0] != matrix.shape[1]:
+        return "matrix is not a numpy.ndarray"
+    if matrix.size == 0 or matrix.shape[0] != matrix.shape[1]:
         return None
     if not np.allclose(matrix, matrix.T):
-        return None
+        return "matrix is not symmetric"
 
     eigenvalues = np.linalg.eigvals(matrix)
-    if np.all(eigenvalues > 0):
-        return "Positive definite"
-    if np.all(eigenvalues >= 0):
-        return "Positive semi-definite"
-    if np.all(eigenvalues < 0):
-        return "Negative definite"
-    if np.all(eigenvalues <= 0):
-        return "Negative semi-definite"
+    all_positive = np.all(eigenvalues > 0)
+    all_nonnegative = np.all(eigenvalues >= 0)
+    all_negative = np.all(eigenvalues < 0)
+    all_nonpositive = np.all(eigenvalues <= 0)
+
+    if all_positive:
+        return "matrix is positive definite"
+    if all_nonnegative and not all_positive:
+        return "matrix is positive semi-definite"
+    if all_negative:
+        return "matrix is negative definite"
+    if all_nonpositive and not all_negative:
+        return "matrix is negative semi-definite"
     if np.any(eigenvalues > 0) and np.any(eigenvalues < 0):
-        return "Indefinite"
+        return "matrix is indefinite"
     return None
-
-
-if __name__ == '__main__':
-    import numpy as np
-
-    # Test cases as provided
-    mat1 = np.array([[5, 1], [1, 1]])
-    mat2 = np.array([[2, 4], [4, 8]])
-    mat3 = np.array([[-1, 1], [1, -1]])
-    mat4 = np.array([[-2, 4], [4, -9]])
-    mat5 = np.array([[1, 2], [2, 1]])
-    mat6 = np.array([])
-    mat7 = np.array([[1, 2, 3], [4, 5, 6]])
-    mat8 = [[1, 2], [1, 2]]
-
-    print(definiteness(mat1))
-    print(definiteness(mat2))
-    print(definiteness(mat3))
-    print(definiteness(mat4))
-    print(definiteness(mat5))
-    print(definiteness(mat6))
-    print(definiteness(mat7))
-    try:
-        definiteness(mat8)
-    except Exception as e:
-        print(e)
