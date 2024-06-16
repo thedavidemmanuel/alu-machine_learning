2bce7a57d7cf5a0591cf259e2903687cf619cbbd
thedavidemmanuel <sirdavidemmanuel@gmail.com>
2024-06-16 09:22:28 +0200

    model

--- supervised_learning/error_analysis/0-create_confusion.py
+++ supervised_learning/error_analysis/0-create_confusion.py
@@ -3,17 +3,21 @@
 
 import numpy as np
 
+
 def create_confusion_matrix(labels, logits):
     """
     Creates a confusion matrix.
-    
+
     Parameters:
-    - labels: is a one-hot numpy.ndarray of shape (m, classes) containing the correct labels for each data point
-    - logits: is a one-hot numpy.ndarray of shape (m, classes) containing the predicted labels
-    
+    - labels: is a one-hot numpy.ndarray of shape (m, classes) containing the
+      correct labels for each data point
+    - logits: is a one-hot numpy.ndarray of shape (m, classes) containing the
+      predicted labels
+
     Returns:
-    A confusion numpy.ndarray of shape (classes, classes) with row indices representing the correct labels
-    and column indices representing the predicted labels.
+    A confusion numpy.ndarray of shape (classes, classes) with row indices
+    representing the correct labels and column indices representing the
+    predicted labels.
     """
     m, classes = labels.shape
     confusion = np.zeros((classes, classes))
