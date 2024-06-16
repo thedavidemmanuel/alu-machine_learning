4e7a0af6fb670d228cc36ab5a22222c99035748f
thedavidemmanuel <sirdavidemmanuel@gmail.com>
2024-06-16 09:26:56 +0200

    model

--- supervised_learning/error_analysis/1-sensitivity.py
+++ supervised_learning/error_analysis/1-sensitivity.py
@@ -10,8 +10,8 @@ def sensitivity(confusion):
 
     Parameters:
     - confusion: is a confusion numpy.ndarray of shape (classes, classes) where
-      row indices represent the correct labels and column indices represent the
-      predicted labels
+      row indices represent the correct labels and column 
+      indices represent the predicted labels
 
     Returns:
     A numpy.ndarray of shape (classes,) containing the sensitivity of each class
