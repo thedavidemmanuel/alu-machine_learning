82e1e75c89a99c50f71a70d97afa5e4abecaddd8
thedavidemmanuel <sirdavidemmanuel@gmail.com>
2024-06-16 09:37:09 +0200

    model

--- supervised_learning/error_analysis/3-specificity.py
+++ supervised_learning/error_analysis/3-specificity.py
@@ -21,7 +21,7 @@ def specificity(confusion):
     false_positives = np.sum(confusion, axis=0) - true_positives
     false_negatives = np.sum(confusion, axis=1) - true_positives
     true_negatives = np.sum(confusion) - \
-    (false_positives + false_negatives + true_positives)
+        (false_positives + false_negatives + true_positives)
 
     specificity = true_negatives / (true_negatives + false_positives)
     return specificity
