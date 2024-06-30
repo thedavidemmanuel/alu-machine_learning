48ac4551758188ac8d922b72ee99d9c803f6e8af
David Emmanuel <sirdavidemmanuel@gmail.com>
2024-07-01 00:56:38 +0200

    apis

--- pipeline/apis/4-rocket_frequency.py
+++ pipeline/apis/4-rocket_frequency.py
@@ -4,8 +4,10 @@ Script that displays the number of launches per rocket using the SpaceX API.
 """
 
 import requests
+from collections import Counter
 
-def main():
+
+def get_launch_count_by_rocket():
     """
     Fetches launch and rocket data from SpaceX API, counts the number of launches per rocket,
     and displays the results sorted by the number of launches in descending order.
@@ -19,26 +21,24 @@ def main():
 
     if launches_response.status_code != 200 or rockets_response.status_code != 200:
         print("Failed to fetch data from SpaceX API")
-        return
+        return []
 
     launches = launches_response.json()
     rockets = rockets_response.json()
 
     rocket_id_to_name = {rocket['id']: rocket['name'] for rocket in rockets}
-    rocket_launch_counts = {}
+    rocket_launch_counts = Counter(launch['rocket'] for launch in launches)
 
-    for launch in launches:
-        rocket_id = launch['rocket']
-        rocket_name = rocket_id_to_name.get(rocket_id, "Unknown")
-        if rocket_name in rocket_launch_counts:
-            rocket_launch_counts[rocket_name] += 1
-        else:
-            rocket_launch_counts[rocket_name] = 1
+    rocket_launch_counts_sorted = sorted(
+        ((rocket_id_to_name[rocket_id], count)
+         for rocket_id, count in rocket_launch_counts.items()),
+        key=lambda x: (-x[1], x[0])
+    )
 
-    sorted_launch_counts = sorted(rocket_launch_counts.items(), key=lambda x: (-x[1], x[0]))
+    return rocket_launch_counts_sorted
 
-    for rocket_name, count in sorted_launch_counts:
-        print("{}: {}".format(rocket_name, count))
 
 if __name__ == "__main__":
-    main()
+    rocket_launch_counts = get_launch_count_by_rocket()
+    for rocket_name, count in rocket_launch_counts:
+        print("{}: {}".format(rocket_name, count))
