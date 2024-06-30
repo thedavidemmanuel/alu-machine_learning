8fa676acfbf5ebd3e0aff3bb0c005ac4f5a78077
David Emmanuel <sirdavidemmanuel@gmail.com>
2024-07-01 00:58:12 +0200

    apis

--- pipeline/apis/4-rocket_frequency.py
+++ pipeline/apis/4-rocket_frequency.py
@@ -19,19 +19,28 @@ def get_launch_count_by_rocket():
     launches_response = requests.get(launches_url)
     rockets_response = requests.get(rockets_url)
 
-    if launches_response.status_code != 200 or rockets_response.status_code != 200:
+    if (launches_response.status_code != 200 or
+            rockets_response.status_code != 200):
         print("Failed to fetch data from SpaceX API")
         return []
 
     launches = launches_response.json()
     rockets = rockets_response.json()
 
-    rocket_id_to_name = {rocket['id']: rocket['name'] for rocket in rockets}
-    rocket_launch_counts = Counter(launch['rocket'] for launch in launches)
+    rocket_id_to_name = {
+        rocket['id']: rocket['name']
+        for rocket in rockets
+    }
+    rocket_launch_counts = Counter(
+        launch['rocket']
+        for launch in launches
+    )
 
     rocket_launch_counts_sorted = sorted(
-        ((rocket_id_to_name[rocket_id], count)
-         for rocket_id, count in rocket_launch_counts.items()),
+        (
+            (rocket_id_to_name[rocket_id], count)
+            for rocket_id, count in rocket_launch_counts.items()
+        ),
         key=lambda x: (-x[1], x[0])
     )
 
