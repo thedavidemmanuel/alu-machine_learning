7f67542d8202f1c860001955603c16741e42c8c6
David Emmanuel <sirdavidemmanuel@gmail.com>
2024-07-01 00:10:29 +0200

    api

--- pipeline/apis/4-rocket_frequency.py
+++ pipeline/apis/4-rocket_frequency.py
@@ -1,47 +1,22 @@
 #!/usr/bin/env python3
-"""
-Script that displays the number of launches per rocket using the SpaceX API.
-"""
-
+"""Pipeline Api"""
 import requests
 
 
-def main():
-    """
-    Fetches and displays the number of launches per rocket.
-    Orders the results by the number of launches (descending) and alphabetically.
-    """
-    rockets_url = "https://api.spacexdata.com/v4/rockets"
-    launches_url = "https://api.spacexdata.com/v4/launches"
+if __name__ == '__main__':
+    """pipeline api"""
+    url = "https://api.spacexdata.com/v4/launches"
+    r = requests.get(url)
+    rocket_dict = {"5e9d0d95eda69955f709d1eb": 0}
 
-    rockets_response = requests.get(rockets_url)
-    launches_response = requests.get(launches_url)
-
-    if rockets_response.status_code != 200 or launches_response.status_code != 200:
-        print("Failed to fetch data from SpaceX API")
-        return
-
-    rockets = rockets_response.json()
-    launches = launches_response.json()
-
-    rocket_launch_counts = {}
-
-    for launch in launches:
-        rocket_id = launch['rocket']
-        if rocket_id in rocket_launch_counts:
-            rocket_launch_counts[rocket_id] += 1
+    for launch in r.json():
+        if launch["rocket"] in rocket_dict:
+            rocket_dict[launch["rocket"]] += 1
         else:
-            rocket_launch_counts[rocket_id] = 1
+            rocket_dict[launch["rocket"]] = 1
+    for key, value in sorted(rocket_dict.items(),
+                             key=lambda kv: kv[1], reverse=True):
+        rurl = "https://api.spacexdata.com/v4/rockets/" + key
+        req = requests.get(rurl)
 
-    rocket_names = {rocket['id']: rocket['name'] for rocket in rockets}
-
-    launch_counts = [(rocket_names[rocket_id], count) for rocket_id, count in rocket_launch_counts.items()]
-
-    sorted_launch_counts = sorted(launch_counts, key=lambda x: (-x[1], x[0]))
-
-    for rocket_name, count in sorted_launch_counts:
-        print(f"{rocket_name}: {count}")
-
-
-if __name__ == "__main__":
-    main()
+        print(req.json()["name"] + ": " + str(value))
