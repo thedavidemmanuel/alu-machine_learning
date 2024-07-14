37541a2c96f51512921a09f746fc4ecbe11dec39
thedavidemmanuel <sirdavidemmanuel@gmail.com>
2024-07-14 23:27:04 +0200

    databases

--- pipeline/databases/34-log_stats.py
+++ pipeline/databases/34-log_stats.py
@@ -3,12 +3,9 @@
 Nginx logs stored in MongoDB:
 """
 
-
 from pymongo import MongoClient
 
-
 if __name__ == '__main__':
-
     client = MongoClient('mongodb://127.0.0.1:27017')
     collection = client.logs.nginx
 
@@ -16,12 +13,10 @@ if __name__ == '__main__':
     total_logs = collection.count_documents({})
 
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    method_counts = {method: collection.count_documents(
-        {"method": method}) for method in methods}
+    method_counts = {method: collection.count_documents({"method": method}) for method in methods}
 
     # Get the count of status check
-    status_check_count = collection.count_documents(
-        {"method": "GET", "path": "/status"})
+    status_check_count = collection.count_documents({"method": "GET", "path": "/status"})
 
     # Print the stats
     print(f"{total_logs} logs")
