25c225804f6e76c46bb791d9a28af56ba702346a
David Emmanuel <38293570+thedavidemmanuel@users.noreply.github.com>
2024-08-05 08:15:37 +0000

    updates

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -1,48 +1,47 @@
 #!/usr/bin/env python3
-"""
-Script to print the location of a specific GitHub user using the GitHub API.
-"""
+'''
+Prints the location of a user
+'''
+
 
 import sys
 import requests
-from datetime import datetime
+import time
 
 
 def get_user_location(api_url):
     """
     Fetch and print the location of a GitHub user.
 
-    Args:
-        api_url (str): The full GitHub API URL for the user.
-
-    Returns:
-        None
+    :param api_url: The API URL for the user
     """
-    headers = {
-        'Accept': 'application/vnd.github+json',
-        'X-GitHub-Api-Version': '2022-11-28'
-    }
+    try:
+        response = requests.get(api_url)
 
-    response = requests.get(api_url, headers=headers)
-
-    if response.status_code == 200:
-        user_data = response.json()
-        location = user_data.get('location', 'No location provided')
-        print(location)
-    elif response.status_code == 404:
-        print("Not found")
-    elif response.status_code == 403:
-        reset_time = int(response.headers.get('X-Ratelimit-Reset', 0))
-        current_time = int(datetime.now().timestamp())
-        minutes_left = (reset_time - current_time) // 60
-        print("Reset in {} min".format(minutes_left))
-    else:
-        print("An error occurred: Status code {}".format(response.status_code))
+        if response.status_code == 200:
+            user_data = response.json()
+            location = user_data.get('location')
+            if location:
+                print(location)
+            else:
+                print('Location not available')
+        elif response.status_code == 404:
+            print('Not found')
+        elif response.status_code == 403:
+            reset_time = int(
+                response.headers.get('X-RateLimit-Reset', time.time()))
+            current_time = int(time.time())
+            wait_time = (reset_time - current_time) // 60
+            print('Reset in {} min'.format(wait_time))
+        else:
+            print('Error: {}'.format(response.status_code))
+    except requests.RequestException as e:
+        print('An error occurred: {}'.format(e))
 
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
-        print("Usage: ./2-user_location.py <github_api_user_url>")
+        print('Usage: ./2-user_location.py <api_url>')
         sys.exit(1)
 
     api_url = sys.argv[1]
