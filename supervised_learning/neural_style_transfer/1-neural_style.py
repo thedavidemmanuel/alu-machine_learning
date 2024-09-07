73b1ae687109b4a2ea034e654d9cadfe39592bae
thedavidemmanuel <sirdavidemmanuel@gmail.com>
2024-09-07 21:29:01 +0200

    neural-style-transfer

--- supervised_learning/neural_style_transfer/1-neural_style.py
+++ supervised_learning/neural_style_transfer/1-neural_style.py
@@ -16,10 +16,16 @@ class NST:
         """
         Class constructor for Neural Style Transfer
         """
-        if not isinstance(style_image, np.ndarray) or style_image.shape[-1] != 3:
-            raise TypeError("style_image must be a numpy.ndarray with shape (h, w, 3)")
-        if not isinstance(content_image, np.ndarray) or content_image.shape[-1] != 3:
-            raise TypeError("content_image must be a numpy.ndarray with shape (h, w, 3)")
+        if not isinstance(style_image, np.ndarray) or \
+           style_image.shape[-1] != 3:
+            raise TypeError(
+                "style_image must be a numpy.ndarray with shape (h, w, 3)"
+            )
+        if not isinstance(content_image, np.ndarray) or \
+           content_image.shape[-1] != 3:
+            raise TypeError(
+                "content_image must be a numpy.ndarray with shape (h, w, 3)"
+            )
         if not isinstance(alpha, (int, float)) or alpha < 0:
             raise TypeError("alpha must be a non-negative number")
         if not isinstance(beta, (int, float)) or beta < 0:
@@ -40,7 +46,9 @@ class NST:
         and its largest side is 512 pixels
         """
         if not isinstance(image, np.ndarray) or image.shape[-1] != 3:
-            raise TypeError("image must be a numpy.ndarray with shape (h, w, 3)")
+            raise TypeError(
+                "image must be a numpy.ndarray with shape (h, w, 3)"
+            )
 
         h, w, _ = image.shape
         max_dim = max(h, w)
@@ -58,13 +66,32 @@ class NST:
 
     def load_model(self):
         """Creates the model used to calculate cost"""
-        vgg = tf.keras.applications.VGG19(include_top=False, weights='imagenet')
+        vgg = tf.keras.applications.VGG19(include_top=False,
+                                          weights='imagenet')
 
         custom_objects = {'MaxPooling2D': tf.keras.layers.AveragePooling2D}
         vgg.save('tmp_vgg')
-        vgg = tf.keras.models.load_model('tmp_vgg', custom_objects=custom_objects)
+        vgg = tf.keras.models.load_model('tmp_vgg',
+                                         custom_objects=custom_objects)
 
         vgg.trainable = False
         outputs = [vgg.get_layer(name).output for name in self.style_layers]
         outputs.append(vgg.get_layer(self.content_layer).output)
         self.model = tf.keras.models.Model(vgg.input, outputs)
+
+    def print_model_summary(self):
+        """Prints a summary of the model with specific formatting"""
+        line = "_" * 65
+        print(line)
+        print("Layer (type)                 Output Shape              Param #   ")
+        print("=" * 65)
+        for layer in self.model.layers[:6]:  # Print only first 6 layers
+            name = layer.name
+            cls_name = layer.__class__.__name__
+            params = layer.count_params()
+            output_shape = str(layer.output_shape)
+            print("{} ({})".format(name, cls_name).ljust(29) +
+                  "{}".format(output_shape).ljust(25) +
+                  "{}".format(params).ljust(10))
+            print(line)
+        print("..." + " " * 62)  # Add ellipsis at the end
